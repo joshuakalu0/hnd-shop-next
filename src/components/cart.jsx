43cc4c99c963cfb423e4504e8911364dc8421afc
Aju Joshua kalu <69370514+joshuakalu0@users.noreply.github.com>
2024-07-10 12:02:59 +0100

    Update cart.jsx
    Bb

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -6,7 +6,7 @@ import { SingleCartItem } from "./product/singleCartItem";
 export const Cart = () => {
   const { cartobject, setcartobject } = useContext(CartContext);
   return (
-    <div className="h-screen p-10">
+    <div className="min-h-screen p-10">
       <span>Home {">"} Cart</span>
       <h1 className="mb-10 pl-2 pt-3 text-2xl font-extrabold">Cart Items</h1>
       <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
